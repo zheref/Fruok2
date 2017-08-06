67ff6c833e86f63c1c6bb623b93c6c3278121522
Matthias Keiser <matthias@tristan-inc.com>
2017-08-06 19:58:56 +0200

    Initial Commit

---
+++ Fruok/Document.swift
@@ -0,0 +1,29 @@
+//
+//  Document.swift
+//  Fruok
+//
+//  Created by Matthias Keiser on 06.08.17.
+//  Copyright © 2017 Tristan Inc. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+	override init() {
+	    super.init()
+		// Add your subclass-specific initialization here.
+	}
+
+	override class func autosavesInPlace() -> Bool {
+		return true
+	}
+
+	override func makeWindowControllers() {
+		// Returns the Storyboard that contains your Document window.
+		let storyboard = NSStoryboard(name: "Main", bundle: nil)
+		let windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
+		self.addWindowController(windowController)
+	}
+
+}
---
+++ Fruok/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Fruok
+//
+//  Created by Matthias Keiser on 06.08.17.
+//  Copyright © 2017 Tristan Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+

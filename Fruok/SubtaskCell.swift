a8677103444b4605ca218042203b730c53d8e894
Matthias Keiser <matthias@tristan-inc.com>
2017-08-18 01:06:18 +0200

    SubtaskCell: fix memory leak.

--- Fruok/SubtaskCell.swift
+++ Fruok/SubtaskCell.swift
@@ -37,25 +37,25 @@ class SubtaskCell: NSTableCellView, MVVMView {
 
 	func connectVM() {
 
-		self.viewModel?.done.map{ $0 ? NSOnState : NSOffState}.bind(to: self.doneCheckbox.reactive.state).dispose(in: reuseBag)
-		self.doneCheckbox.reactive.state.map{ $0 == NSOnState ? true : false }.bind(to: self.viewModel!.done).dispose(in: reuseBag)
+		self.viewModel?.done.map{ $0 ? NSOnState : NSOffState}.bind(to: self.doneCheckbox.reactive.state)
+		self.doneCheckbox.reactive.state.map{ $0 == NSOnState ? true : false }.bind(to: self.viewModel!.done)
 
-		self.viewModel?.name.bind(to: self.nameLabel.reactive.stringValue).dispose(in: reuseBag)
-		self.viewModel!.editable.observeNext(with: {editable in
+		self.viewModel?.name.bind(to: self.nameLabel.reactive.stringValue)
+		self.viewModel!.editable.observeNext(with: { [weak self] editable in
 
 			if editable {
-				self.nameLabel.isHidden = true
-				self.nameEditingField.isHidden = false
-				self.window?.makeFirstResponder(self.nameEditingField)
+				self?.nameLabel.isHidden = true
+				self?.nameEditingField.isHidden = false
+				self?.window?.makeFirstResponder(self?.nameEditingField)
 			} else {
-				self.nameLabel.isHidden = false
-				self.nameEditingField.isHidden = true
+				self?.nameLabel.isHidden = false
+				self?.nameEditingField.isHidden = true
 			}
 		}).dispose(in: reuseBag)
 
-		self.nameEditingField.reactive.textDidEndEditing.observeNext { (textField, flag) in
-			self.viewModel?.name.value = textField.stringValue
-			self.viewModel?.userWantsEndEditing()
+		self.nameEditingField.reactive.textDidEndEditing.observeNext { [weak self] (textField, flag) in
+			self?.viewModel?.name.value = textField.stringValue
+			self?.viewModel?.userWantsEndEditing()
 		}.dispose(in: reuseBag)
 
 		self.reactive.keyPath(#keyPath(SubtaskCell.window), ofType: Optional<NSWindow>.self).bind(to: self, context: .immediateOnMain) { (me, window) in
@@ -63,7 +63,7 @@ class SubtaskCell: NSTableCellView, MVVMView {
 			if (me.viewModel?.editable.value ?? false) {
 				window?.makeFirstResponder(me.nameEditingField)
 			}
-		}.dispose(in: reuseBag)
+		}
 	}
 
 	@IBAction override func cancelOperation(_ sender: Any?) {

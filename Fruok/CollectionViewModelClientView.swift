f2b942326c744b03e66c960b8b366186ed14e352
Matthias Keiser <matthias@tristan-inc.com>
2017-08-21 13:18:40 +0200

    Disabling animations.

--- Fruok/CollectionViewModelClientView.swift
+++ Fruok/CollectionViewModelClientView.swift
@@ -19,61 +19,64 @@ extension CollectionViewModelClientView {
 
 	func executeCollectionViewModelAction(_ action: CollectionViewModelActions?) {
 
-		switch action {
-		case .refreshTaskStates?:
-			self.collectionView.reloadData()
-
-		case .addTasksAtIndexes(let indexSet)?:
-
-			let set = Set(indexSet.map({IndexPath(item: $0, section: 0)}))
-			self.collectionView.animator().performBatchUpdates({
-
-				NSAnimationContext.current().allowsImplicitAnimation = true
-				self.collectionView.insertItems(at: set)
-
-			}, completionHandler: { _ in
-
-				let liveSet = Set(set.filter{ $0.item < self.collectionView.numberOfItems(inSection: 0) })
-				if liveSet.count > 0 {
-					NSAnimationContext.current().allowsImplicitAnimation = true
-					self.collectionView.scrollToItems(at: set, scrollPosition: .centeredHorizontally)
-				}
-			})
-		case .deleteTasksAtIndexes(let indexSet)?:
-			self.collectionView.animator().performBatchUpdates({
-
-				let set = Set(indexSet.map({IndexPath(item: $0, section: 0)}))
-				self.collectionView.deleteItems(at: set)
-
-			}, completionHandler: nil)
-		case .moveTasks(let mappingDict)?:
-
-			let targetPaths = mappingDict.values.reduce(Set<IndexPath>(), { (set, index) in
-				var setCopy = set
-				setCopy.insert(IndexPath(item: index, section: 0))
-				return setCopy
-			})
-
-			if let draggedIndexPaths = self.draggedIndexPaths() {
-
-				for item in draggedIndexPaths.map({self.collectionView.item(at: $0)}) {
-					item?.view.alphaValue = 0.0
-				}
-				(self.collectionView.collectionViewLayout as? DragAndDropCollectionLayout)?.hiddenIndexPaths = targetPaths
-			}
-			self.collectionView.animator().performBatchUpdates({
-
-				for (source, target) in mappingDict {
-					self.collectionView.moveItem(at: IndexPath(item: source, section: 0), to: IndexPath(item: target, section: 0))
-				}
-			}, completionHandler: { _ in
-
-				(self.collectionView.collectionViewLayout as? DragAndDropCollectionLayout)?.hiddenIndexPaths = nil
-				self.collectionView.collectionViewLayout?.invalidateLayout()
-			})
-		case nil:
-			break
-		}
+		self.collectionView.reloadData()
+		return
+		
+//		switch action {
+//		case .refreshTaskStates?:
+//			self.collectionView.reloadData()
+//
+//		case .addTasksAtIndexes(let indexSet)?:
+//
+//			let set = Set(indexSet.map({IndexPath(item: $0, section: 0)}))
+//			self.collectionView.animator().performBatchUpdates({
+//
+//				NSAnimationContext.current().allowsImplicitAnimation = true
+//				self.collectionView.insertItems(at: set)
+//
+//			}, completionHandler: { _ in
+//
+//				let liveSet = Set(set.filter{ $0.item < self.collectionView.numberOfItems(inSection: 0) })
+//				if liveSet.count > 0 {
+//					NSAnimationContext.current().allowsImplicitAnimation = true
+//					self.collectionView.scrollToItems(at: set, scrollPosition: .centeredHorizontally)
+//				}
+//			})
+//		case .deleteTasksAtIndexes(let indexSet)?:
+//			self.collectionView.animator().performBatchUpdates({
+//
+//				let set = Set(indexSet.map({IndexPath(item: $0, section: 0)}))
+//				self.collectionView.deleteItems(at: set)
+//
+//			}, completionHandler: nil)
+//		case .moveTasks(let mappingDict)?:
+//
+//			let targetPaths = mappingDict.values.reduce(Set<IndexPath>(), { (set, index) in
+//				var setCopy = set
+//				setCopy.insert(IndexPath(item: index, section: 0))
+//				return setCopy
+//			})
+//
+//			if let draggedIndexPaths = self.draggedIndexPaths() {
+//
+//				for item in draggedIndexPaths.map({self.collectionView.item(at: $0)}) {
+//					item?.view.alphaValue = 0.0
+//				}
+//				(self.collectionView.collectionViewLayout as? DragAndDropCollectionLayout)?.hiddenIndexPaths = targetPaths
+//			}
+//			self.collectionView.animator().performBatchUpdates({
+//
+//				for (source, target) in mappingDict {
+//					self.collectionView.moveItem(at: IndexPath(item: source, section: 0), to: IndexPath(item: target, section: 0))
+//				}
+//			}, completionHandler: { _ in
+//
+//				(self.collectionView.collectionViewLayout as? DragAndDropCollectionLayout)?.hiddenIndexPaths = nil
+//				self.collectionView.collectionViewLayout?.invalidateLayout()
+//			})
+//		case nil:
+//			break
+//		}
 	}
 
 }

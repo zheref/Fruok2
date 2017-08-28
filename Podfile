e235ce8c8df4602abde31d337435aa92bf13ea42
Matthias Keiser <matthias@tristan-inc.com>
2017-08-28 23:05:20 +0200

    Add Charts dependency

--- Podfile
+++ Podfile
@@ -9,7 +9,8 @@ target 'Fruok' do
   use_frameworks!
 
   # Pods for Fruok
-  pod 'Bond'
+  pod 'Bond', '6.2.6'
+  pod 'Charts', '~> 3.0.2'
 
   target 'FruokTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-Fruok/Pods-Fruok-frameworks.sh
+++ Pods/Target Support Files/Pods-Fruok/Pods-Fruok-frameworks.sh
@@ -90,11 +90,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bond/Bond.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Charts/Charts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Diff/Diff.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveKit/ReactiveKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bond/Bond.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Charts/Charts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Diff/Diff.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveKit/ReactiveKit.framework"
 fi
